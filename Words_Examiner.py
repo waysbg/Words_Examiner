afcb08e4b2a72433a80645415a7c6e63367aa6fe
waysbg <waysbg@gmail.com>
2022-09-23 15:30:31 +0300

    Included Bulgarian translation speech synthesizer

--- Words_Examiner.py
+++ Words_Examiner.py
@@ -3,9 +3,11 @@ from ast import literal_eval
 from gtts import gTTS
 from playsound import playsound
 from pathlib import Path
+from time import sleep
 
 welcome = Path().cwd() / "welcome.mp3"
 playsound(welcome)
+sleep(1)
 
 file = open("dictionary.txt")
 data = file.read()
@@ -18,12 +20,17 @@ while True:
     word_to_play.save("word.mp3")
     word_to_play = Path().cwd() / "word.mp3"
     playsound(word_to_play)
+
     decision = str(input(f"[t]ranslate, [n]ext or [e]xit           {picked_word}? "))
+    sleep(1)
+
     if decision == "e":
         break
     elif decision == "t":
-        print(f"{' ' * 40}{dictionary[picked_word]}\n")
-
-
-
-
+        translation = dictionary[picked_word]
+        print(f"{' ' * 40}{translation}\n")
+        translation_to_play = gTTS(text=translation, lang='bg')
+        translation_to_play.save("translation.mp3")
+        translation_to_play = Path().cwd() / "translation.mp3"
+        playsound(translation_to_play)
+        sleep(1)
