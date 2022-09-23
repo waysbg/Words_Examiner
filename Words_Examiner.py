3b057ecd90141837b833c81aff133586b1fb139a
waysbg <waysbg@gmail.com>
2022-09-23 14:30:23 +0300

    Included speech synthesizer

--- Words_Examiner.py
+++ Words_Examiner.py
@@ -1,5 +1,11 @@
 from random import choice
 from ast import literal_eval
+from gtts import gTTS
+from playsound import playsound
+from pathlib import Path
+
+welcome = Path().cwd() / "welcome.mp3"
+playsound(welcome)
 
 file = open("dictionary.txt")
 data = file.read()
@@ -8,11 +14,15 @@ english_words = list(dictionary.keys())
 
 while True:
     picked_word = choice(english_words)
-    decision = str(input(f"o[p]en, [n]ext or [e]xit           {picked_word}? "))
+    word_to_play = gTTS(text=picked_word, lang='en')
+    word_to_play.save("word.mp3")
+    word_to_play = Path().cwd() / "word.mp3"
+    playsound(word_to_play)
+    decision = str(input(f"[t]ranslate, [n]ext or [e]xit           {picked_word}? "))
     if decision == "e":
         break
-    elif decision == "p":
-        print(f"{' ' * 40}{dictionary[picked_word]}")
+    elif decision == "t":
+        print(f"{' ' * 40}{dictionary[picked_word]}\n")
 
 
 
